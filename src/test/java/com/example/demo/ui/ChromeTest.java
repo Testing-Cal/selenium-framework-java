9f057ad7312aa3883127389c68317ef18341a2e5
ElabTemplateAdmin <elabtemplateadmin@altimetrik.com>
2022-02-07 05:32:19 +0000

    Update ChromeTest.java

--- src/test/java/com/example/demo/ui/ChromeTest.java
+++ src/test/java/com/example/demo/ui/ChromeTest.java
@@ -11,7 +11,7 @@ public class ChromeTest extends BaseTest {
 	        System.out.println("Test Started! " );
 			String siteURL = "http://52.91.78.138:3015/api";
 			System.out.println("Using site URL: " + siteURL);
-	        getDriver().navigate().to(System.getProperty("SITE_URL"));
+	        getDriver().navigate().to(siteURL);
 	        System.out.println("Test's Page title is: " + getDriver().getTitle());
 	        Assert.assertEquals(getDriver().getTitle(), "Google");
 	        System.out.println("Test Ended! ");
