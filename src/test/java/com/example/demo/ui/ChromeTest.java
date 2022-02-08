2d67f107a4fcb1f2460afe4175ffe2fd4f9a0c2b
ElabTemplateAdmin <elabtemplateadmin@altimetrik.com>
2022-02-08 07:49:33 +0000

    Update ChromeTest.java

--- src/test/java/com/example/demo/ui/ChromeTest.java
+++ src/test/java/com/example/demo/ui/ChromeTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 public class ChromeTest extends BaseTest {
 	
 	    @Test
-	    public void GSAMPLE1() {
+	    public void GSAMPLE1() throws Exception {
 	        System.out.println("Test Started! " );
 			String siteURL = "http://54.175.11.133:3015/api/";
 			System.out.println("Using site URL: " + siteURL);
@@ -17,6 +17,7 @@ public class ChromeTest extends BaseTest {
 	        System.out.println("Test's Page title is: " + getDriver().getTitle());
 	        Assert.assertEquals(getDriver().getTitle(), titileToBeCompared);
 	        System.out.println("Test Complete! ");
+			Thread.sleep(5000);
 	    }
 	
 }
