25b7e2410d7f899de0c0bb7eafe93d0d9525d055
“Sagar <srajput@calibo.com>
2022-04-20 18:16:30 +0530

    Updated Chrome test

--- src/test/java/com/example/demo/ui/ChromeTest.java
+++ src/test/java/com/example/demo/ui/ChromeTest.java
@@ -9,124 +9,115 @@ import org.testng.annotations.Test;
 
 public class ChromeTest extends BaseTest {
 	
-/*	    @Test
-	    public void GSAMPLE1() throws InterruptedException {
-			try {
-				System.out.println("Test Started! ");
-				//String siteURL = "http://20.232.193.80:3015/api/";
-				String siteURL = System.getProperty("SITE_URL");
-				if (siteURL == null) {
-					// Pass the Site Under Test URL here in below format
-					// http://209.12.12.12:3000/api/
-					siteURL = "http://<site-under-test-url>:<site-port>/<context-path>/";
-				}
-				System.out.println("Using site URL: " + siteURL);
-				String titileToBeCompared = "React 17.0.2";
-				System.out.println("Site title to be compared: " + titileToBeCompared);
-				//Thread.sleep(10000);
-				getDriver().navigate().to(siteURL);
-				System.out.println("Test's Page title is: " + getDriver().getTitle());
-				Assert.assertEquals(getDriver().getTitle(), titileToBeCompared);
-				System.out.println("Test Complete! ");
-				//Thread.sleep(5000);
+	@Test
+	public void GSAMPLE1() throws InterruptedException {
+			System.out.println("Test Started! ");
+			//String siteURL = "http://20.232.193.80:3015/api/";
+			String siteURL = System.getProperty("SITE_URL");
+			if (siteURL == null) {
+				// Pass the Site Under Test URL here in below format
+				// http://209.12.12.12:3000/api/
+				siteURL = "http://<site-under-test-url>:<site-port>/<context-path>/";
 			}
-			catch(Exception e){
-
-
-			}
-		}*/
-
-	@Test
-	public void GSAMPLE2() throws InterruptedException {
-		Assert.assertEquals("sagar","sagar");
-
+			System.out.println("Using site URL: " + siteURL);
+			//String titileToBeCompared = "React 17.0.2";
+			String titileToBeCompared = "Angular 12";
+			System.out.println("Site title to be compared: " + titileToBeCompared);
+			Thread.sleep(10000);
+			getDriver().navigate().to(siteURL);
+			System.out.println("Test's Page title is: " + getDriver().getTitle());
+			Assert.assertEquals(getDriver().getTitle(), titileToBeCompared);
+			System.out.println("Test Complete! ");
+			Thread.sleep(5000);
 	}
 
 	@Test
-	public void GSAMPLE3() throws InterruptedException {
-		Assert.assertEquals("sagar","sagar");
-	}
-
-	@Test
-	public void GSAMPLE4() throws InterruptedException {
+	public void GSAMPLE2() {
 		Assert.assertEquals("sagar","sagar");
 	}
 
 	@Test
-	public void GSAMPLE5() throws InterruptedException {
+	public void GSAMPLE3() {
 		Assert.assertEquals("sagar","sagar");
 	}
 
 	@Test
-	public void GSAMPLE6() throws InterruptedException {
+	public void GSAMPLE4() {
 		Assert.assertEquals("sagar","sagar");
 	}
 
 	@Test
-	public void GSAMPLE7() throws InterruptedException {
+	public void GSAMPLE5()  {
 		Assert.assertEquals("sagar","sagar");
 	}
 
 	@Test
-	public void GSAMPLE8() throws InterruptedException {
+	public void GSAMPLE6()  {
+		Assert.assertEquals("sagar","sagar");
+	}
+
+	@Test
+	public void GSAMPLE7() {
+		Assert.assertEquals("sagar","sagar");
+	}
+
+	@Test
+	public void GSAMPLE8()  {
 		Assert.assertEquals("sagar1","sagar");
 	}
 
 	@Test
-	public void GSAMPLE9() throws InterruptedException {
+	public void GSAMPLE9()  {
 		Assert.assertEquals("sagar1","sagar");
 	}
 
 	@Test
-	public void GSAMPLE10() throws InterruptedException {
+	public void GSAMPLE10()  {
 		Assert.assertEquals("sagar1","sagar");
 	}
 
 	@Test
-	public void GSAMPLE11() throws InterruptedException {
+	public void GSAMPLE11() {
 		Assert.assertEquals("sagar1","sagar");
 	}
 
 	@Test
-	public void GSAMPLE12() throws InterruptedException {
-		int a =1/0;
+	public void GSAMPLE12() {
+		//int a =1/0;
 	}
 
 	@Test
-	public void GSAMPLE13() throws InterruptedException {
-		int a =1/0;
+	public void GSAMPLE13() {
+		Assert.assertEquals("sagar","sagar");
 	}
 
 	@Test
-	public void GSAMPLE14() throws InterruptedException {
-		int a =1/0;
+	public void GSAMPLE14() {
+		Assert.assertEquals("sagar1","sagar");
 	}
 
 	@Test
-	public void GSAMPLE15() throws InterruptedException {
-		int a =1/0;
+	public void GSAMPLE15()	 {
+		Assert.assertEquals("sagar","sagar");
 	}
 
 	@Test
-	public void GSAMPLE16() throws InterruptedException {
-		int a =1/0;
+	public void GSAMPLE16() {
+		Assert.assertEquals("sagar","sagar");
 	}
 
 	@Test
-	public void GSAMPLE17() throws InterruptedException {
-		int a =1/0;
+	public void GSAMPLE17() {
+		Assert.assertEquals("sagar","sagar");
 	}
 
 	@Test
-	public void GSAMPLE18() throws InterruptedException {
-		int a =1/0;
+	public void GSAMPLE18() {
+		Assert.assertEquals("sagar1","sagar");
 	}
 
 	@Test
-	public void GSAMPLE19() throws InterruptedException {
-		int a =1/0;
+	public void GSAMPLE19() {
+		Assert.assertEquals("sagar1","sagar");
 	}
-
-
-	
 }
